1e26c449ddfa37103bd3f4942e08015b5aefa981
Stefan Probst <stefanprobst@posteo.de>
2023-01-12 19:45:39 +0100

    fix: don't remove event listener after invalid post message (e.g. from a browser plugin)

--- src/pages.ts
+++ src/pages.ts
@@ -14,9 +14,9 @@ export function createSuccesPage(args: {
   const origin = new URL(allowedOrigin).origin
   const success = JSON.stringify({ token, provider })
 
-  const title = "Signing in to CMS..."
+  const title = 'Signing in to CMS...'
   const message = title
-  const missingParent = "Cannot find parent window which opened this popup."
+  const missingParent = 'Cannot find parent window which opened this popup.'
 
   return stripIndent(`
     <!doctype html>
@@ -53,11 +53,12 @@ export function createSuccesPage(args: {
             if (event.data !== 'authorizing:${provider}') return
             if (event.origin !== '${origin}') return
 
+            window.removeEventListener('message', receiveMessage)
             window.opener.postMessage('authorization:${provider}:success:${success}', event.origin)
           }
 
           if (window.opener) {
-            window.addEventListener('message', receiveMessage, { once: true })
+            window.addEventListener('message', receiveMessage)
             window.opener.postMessage('authorizing:${provider}', '*')
           } else {
             document.getElementById('message').innerText = '${missingParent}'
@@ -69,12 +70,13 @@ export function createSuccesPage(args: {
   `)
 }
 
-export function createErrorPage(error?: { message: string, url: string }): string {
+export function createErrorPage(error?: { message: string; url: string }): string {
   const title = 'Failed to sign in to CMS'
   let message = title
 
   if (error) {
-    message = title + `. <a href="${error.url}" rel="noreferrer" target="_blank">${error.message}</a>`
+    message =
+      title + `. <a href="${error.url}" rel="noreferrer" target="_blank">${error.message}</a>`
   }
 
   return stripIndent(`
